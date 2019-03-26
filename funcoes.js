14664d3706dc69103b6e614e02309f32b830cd4c
spindola001 <marcos.spindola001@gmail.com>
2019-03-26 00:49:07 -0300

    IntroJScript - Aula

---
+++ funcoes.js
@@ -0,0 +1,6 @@
+function trocaImagem() {
+	document.getElementById("myImage").src="Imagens/HTML_LOGO.png";
+}
+function imagemTroca() {
+	document.getElementById("myImage").src="Imagens/JS_LOGO.png"
+}
